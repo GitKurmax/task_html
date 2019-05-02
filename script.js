6cc099ba5949e9ff142f7f88c92ae5a9668d0dda
Alexandr <alexandrmail32@gmail.com>
2019-05-02 15:06:00 +0300

    Applying of js code for flex container

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function alignLastRow() {
   let items = document.getElementsByTagName('article');
+  let container = document.getElementsByTagName('main');
+  let containerCoords = container[0].getBoundingClientRect();
   let firstElemInRow = items[0].getBoundingClientRect();
-  let lastItem = items[items.length - 1].getBoundingClientRect();
+  let lastElemInRow;
+  let itemCoords;
 
-  for (let item of items) {
-    item.style.marginLeft = 0;
-  }
-
-  for (i = 0; i < items.length; i++) {
-    let elem = items[i].getBoundingClientRect();
-    if (elem.top === lastItem.top) {
-      items[i].style.marginLeft = firstElemInRow.left - 20 + 'px';
+  for (let i = 1; i < items.length; i++) {
+    itemCoords = items[i].getBoundingClientRect();
+    if(itemCoords.top !== firstElemInRow.top){
+      lastElemInRow = items[i-1];
+      let lastElemInRowCoords = lastElemInRow.getBoundingClientRect();
+      let margin = document.body.clientWidth - (lastElemInRowCoords.right + 20 - firstElemInRow.left);
+      container[0].style.marginLeft = margin/2 + 'px';
       return;
-    }  
+    }
   }
 }
 
 alignLastRow();
 window.addEventListener('resize', alignLastRow);
-
